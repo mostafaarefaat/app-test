8c07c8922601ff2f56f3ca48d591839daa555e4c
TRADENET\mrefaat <mrefaat@kn-it.com>
2022-05-17 16:44:09 +0200

    openshift spring boot demo rest app

---
+++ src/main/java/com/example/demo/restcontroller/TestRestController.java
@@ -0,0 +1,16 @@
+package com.example.demo.restcontroller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/app")
+public class TestRestController {
+
+    @GetMapping("/get")
+    public String getMethod(){
+        return "app rest connected";
+    }
+
+}
